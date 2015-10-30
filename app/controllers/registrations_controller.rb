92989fa881a8c6d0eae4a559438b867d1ef78081
bikashp <paudelvikash@gmail.com>
2015-10-29 23:29:32 -0400

    fixed some copy typo and added pg for production

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-     "/two_factor_authenticaitons/new"
+     "/two_factor_authentications/new"
   end
 end
